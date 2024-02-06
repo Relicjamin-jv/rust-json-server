875c04c7be5da8c6880d91c1fb2f2b9b267ced26
Collin Campbell <collincampbell97@proton.me>
2024-02-06 08:31:40 -0500

    Add Doc Strings to Rust Functions
    Documenation is needed for the okapi-rust to work.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,17 @@
 extern crate rocket;
 use rocket::tokio::time::{sleep, Duration};
 
+/// Hello World!
+///
+/// Index page that returns "Hello World"
 #[get("/")]
 fn index() -> &'static str {
     "Hello World"
 }
 
+/// Delay by Seconds
+///
+/// Wait x seconds until respondsing
 #[get("/delay/<seconds>")]
 async fn delay(seconds: u64) -> String {
     sleep(Duration::from_secs(seconds)).await;
